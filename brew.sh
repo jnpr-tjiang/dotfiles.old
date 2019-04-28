f44619a2289d881aeb656bff3658b9e2572308c4
Tong Jiang <tong@Tongs-MacBook-Pro.local>
2019-04-27 21:29:02 -0700

    added brew.sh

---
+++ brew.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+
+# Install Brew Packages
+#brew install python
+brew install tree
+brew install bash-completion
+brew install zsh
+
+# Install MacOS Applications
+brew cask install google-chrome
+#brew cask install firefox
+brew cask install sublime-text
+brew cask install virtualbox
+#brew cask install sourcetree
+#brew cask install spotify
+#brew cask install discord
+#brew cask install google-backup-and-sync
+#brew cask install skype
+#brew cask install gimp
+#brew cask install vlc
+#brew cask install hyperdock
+#brew cask install divvy
+
+# Install Source Code Pro Font
+brew tap homebrew/cask-fonts
+brew cask install font-source-code-pro
